27d03354561e4c24b09c26ccca3ce5b0a4b52af3
briankmbugua <briankmbuguak@gmail.com>
2023-02-07 14:27:42 +0300

    revision

--- pythonDocs/pythonPackages/package.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3.9
-#A module can contain multiple objects such as classes, functions
-# A package can contain one or more relevant modules, Pysically a package is actually a folder containing one or more module files
