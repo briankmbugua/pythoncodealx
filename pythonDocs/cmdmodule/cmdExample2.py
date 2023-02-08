9497c0d30f369edec79110209489ef55136f8366
briankmbugua <briankmbuguak@gmail.com>
2023-02-08 16:19:06 +0300

    cmd module

--- pythonDocs/cmdmodule/cmdExample2.py
+++ pythonDocs/cmdmodule/cmdExample2.py
@@ -5,15 +5,26 @@ import cmd
 
 class HelloWorld(cmd.Cmd):
     """simple command processer example"""#THIS IS A DOCSTRING
+
+    FRIENDS = ['Alice', 'Adam','Barbara','Bob']
     def do_greet(self, person):#if a persons name is provided the greeting is personalized
         """greet [person]
         Greet the named person"""
-        if person:
-            print("Hi", person)
+        if person and person in self.FRIENDS:
+            greeting = f"hi! {person}"
+        elif person:
+            greeting = f"hello {person}"
         else:
-            print("hi")
+            greeting = "Hello"
     def help_greet(self):#The formatting of the help command is not good an alternative solution is to have a help handler named help_greet(). When present the help handler is called on to produce help text for the named command
         print("\n".join([ 'greet [person]', 'Greet the named person']))
+    
+    def complete_greet(self, text, line, begidx, endidx):
+        if not text:
+            completions = self.FRIENDS[:]
+        else:
+            completions = [f for f in self.FRIENDS if f.startswith(text)]
+        return completions
     def do_EOF(self, line):
         """End the program"""
         return True
