903877a7b93baed2c6be52fa167a4204b78736ef
briankmbugua <briankmbuguak@gmail.com>
2023-05-24 23:27:50 +0300

    using engine.connect() with context manager

--- pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/connections.py
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/connections.py
@@ -11,3 +11,6 @@ from engine import engine
 with engine.connect() as conn:
     result = conn.execute(text("select 'hello world'"))
     print(result.all())
+
+#The context manager in this case with provided for a database connection and also framed the operation inside of a transaction. The default behaviour of the Python DBAPI includes that the transaction is always in progress When the scope of the connection is released, a ROLLBACK is emitted to end the transaction.The transaction is not commited automatically when we want to commit data we normally need to call Connection.commit()
+#The result of our SELECT was also returned in an object called Result.It is best to ensure this object is consumed within the connect block and is not passed along outsode the scope of our connection.
