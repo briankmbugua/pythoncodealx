192726caf8295f310c1876d4fa295ecfafbbb3ab
briankmbugua <briankmbuguak@gmail.com>
2023-05-25 23:59:53 +0300

    fetching rows

--- pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/statementexecution.py
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/statementexecution.py
@@ -9,3 +9,18 @@ with engine.connect() as conn:
     result = conn.execute(text("SELECT x, y FROM some_table"))
     for row in result:
         print(f"x: {row.x} y: {row.y}")
+
+# The "SELECT" string we executed selected all rows from our table.The object returned is called Result and represents an iterable object or result rows.
+# Result has a lot of methods for fetching and transforming rows, such as Result.all() method illustrated previously, which returns a list of all Row objects.It also implements the python iterator interface so that we can iterate over the collection of Row objects directly.
+
+# The Row objects themselves are intended to act like Pyhon named tuples.
+
+# Tuple Assingment
+with engine.connect() as connection:
+    result = connection.execute(text("select x, y from some_table"))
+    for x, y in result:
+        print(x)
+        print(y)
+
+
+
