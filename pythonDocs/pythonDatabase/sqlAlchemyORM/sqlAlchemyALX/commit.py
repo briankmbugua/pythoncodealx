6d01c14ac09fea82798c1b36d3b910f4675a0982
briankmbugua <briankmbuguak@gmail.com>
2023-05-25 23:31:03 +0300

    commit method of the connection object

--- pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/commit.py
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/commit.py
@@ -8,3 +8,5 @@ with engine.connect() as conn:
     conn.execute(text("INSERT INTO some_table (x,y) VALUES(:x, :y)"),
                  [{"x":1, "y":1}, {"x":2, "y":4}],)
     conn.commit()
+
+#Above, we emmited two SQL statements that are generally transactional a 'CREATE TABLE' statement and an 'INSERT' statement.As we want the work we've done to be commited within our block we invoke the Connection.commit() method which commits the transaction.After we call this method inside the block, we can continue to run more SQL statements and if we choose we may call Connection.commit() again for subsequent statements.SQLAlchemy refers to this style as commit as you go.
