5c5711eec9695680d45431af1c0473ecf9d391a5
briankmbugua <briankmbuguak@gmail.com>
2023-05-25 23:42:33 +0300

    statement execution

---
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/statementexecution.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#in statement execution we make use of a method called Connection.execute(), in conjuction with an object called text(), and returning an object called Result.
+
+# Fetching Rows
+from engine import engine
+
+with engine.
