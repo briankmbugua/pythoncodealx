855a439341dcad4f980904197f3ddb911601bac5
briankmbugua <briankmbuguak@gmail.com>
2023-05-25 23:32:27 +0300

    engine.begin

---
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/begin.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from engine import engine
+from sqlalchemy import text
+# We can declare our connect block to be a tranasaction block upfront.
+# For this mode of operation we use the Egine.begin() method.This method will both manage the scope of the Connection and also enclose everything inside a transaction with commit at the end assuming a successful block, or ROLLBACK in case of exception raise.
+
+# "begin once"
+with engine.begin as conn:
+    conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),[{"x":6, "y":8}, {"x": 9, "y": 10}],)
