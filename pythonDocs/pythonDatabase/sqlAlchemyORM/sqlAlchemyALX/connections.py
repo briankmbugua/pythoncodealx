8404c491b8d6d764e8322dc745ea7f84f4f3e604
briankmbugua <briankmbuguak@gmail.com>
2023-05-26 18:34:40 +0300

    using engine.connect()

--- pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/connections.py
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/connections.py
@@ -4,8 +4,6 @@
 # The Engine object's main purpose is to provide a connection to the database called a Connection
 # The Connection object is used for all interactions with the database when working with the Core directly
 # It is important to limit the scope of the Connection object to a specific context since the Connection represents an open resource against the database.
-
-from sqlalchemy import create_engine
 from sqlalchemy import text
 from engine import engine
 with engine.connect() as conn:
