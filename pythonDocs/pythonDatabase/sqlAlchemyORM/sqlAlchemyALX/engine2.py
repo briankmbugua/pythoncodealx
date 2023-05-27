f4536cf02f1488aaf0751714a285e8f16b96f958
briankmbugua <briankmbuguak@gmail.com>
2023-05-27 17:07:28 +0300

    engine for the tasks

--- pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/engine2.py
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/engine2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
+import sys
+engine = create_engine('mysql+pymysql://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), echo=True, pool_pre_ping=True)
 
-engine = create_engine('mysql+://root:password@localhost:3306/onedb', echo=True, pool_pre_ping=True)
-
-print(engine)
