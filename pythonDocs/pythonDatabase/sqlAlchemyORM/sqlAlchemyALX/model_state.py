1b55209eaefc3140ab80b6f5027751ef28d7ac8d
briankmbugua <briankmbuguak@gmail.com>
2023-05-30 23:58:44 +0300

    model state

--- pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/model_state.py
+++ pythonDocs/pythonDatabase/sqlAlchemyORM/sqlAlchemyALX/model_state.py
@@ -3,7 +3,7 @@
 Contains class defination of a state and an instane Base = declarative_base();
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -13,7 +13,7 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
+    cities = relationship("City", backref="state")
 
 """
 EXPLANATION FOR THE ABOVE CODE
