ab97c6dff0751bd63a6d6e765525ba2a9f8258fc
briankmbugua <briankmbuguak@gmail.com>
2023-06-03 22:57:01 +0300

    basemodel

--- pythonDocs/AirBnbConsole/models/base_model.py
+++ pythonDocs/AirBnbConsole/models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from models.engine import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -18,17 +17,17 @@ class BaseModel:
             self.created_at = datetime.strptime(kwargs['created_at'],'%Y-%m-%dT%H:%M:%S.%f')
         if 'updated_at' in kwargs:
                 self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        # Add the new instance to the FileStorage
-        storage.new(self)
         # Save the changes to the file
+        from models.engine import storage # lazy import to solve a circular import issue
+        storage.new(self)
         storage.save()
-    
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
     
     def save(self):
         self.updated_at = datetime.now()
         # Save the changes to the file
+        from models.engine import storage
         storage.save()
     
     def to_dict(self):
